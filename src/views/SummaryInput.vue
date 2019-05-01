5c63488d444d16f281eb5abd1834697425edd24f
sa2taka <sa2taka@gmail.com>
2019-05-01 14:12:48 +0900

    追加: nav-barを追加

---
+++ src/views/SummaryInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>SummryInput</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class SummaryInput extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
