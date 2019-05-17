1d8847711f414392b4b9a892f2ad278c7429a98c
sa2taka <sa2taka@gmail.com>
2019-05-17 23:26:31 +0900

    追加: chartを追加

--- src/components/Home/Subject.vue
+++ src/components/Home/Subject.vue
@@ -1,30 +1,98 @@
 <template>
-  <div class="subject">{{subjectRef.id}}: {{resultTime}}</div>
+  <div class="subject">
+    {{subjectRef.id}}: {{resultTime}}
+    <ChartRenderer :chartData="chartData" :option="chartOptions" v-if="canRender"></ChartRenderer>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/auth';
+import Chart from 'chart.js';
 
-@Component
-export default class Home extends Vue {
+import ChartRenderer from '@/components/Common/Chart.vue';
+
+@Component({
+  components: {
+    ChartRenderer,
+  },
+})
+export default class Subject extends Vue {
   public resultTime: string = '';
   @Prop({ required: true })
   public subjectRef!: firebase.firestore.QueryDocumentSnapshot;
 
-  public created() {
-    this.subjectRef.ref
-      .collection('results')
-      .get()
-      .then((snapshot) => {
-        let sum = 0;
-        snapshot.forEach((elm) => {
-          sum += elm.data()!.time;
-        });
-        this.resultTime = String(sum);
-      });
+  public chartData!: Chart.ChartData;
+  public canRender = false;
+
+  public async created() {
+    const snapshot = await this.subjectRef.ref.collection('results').get();
+
+    let sum = 0;
+    const results = this.getInitializedResults();
+    // resultsのkeysからなるArrayを生成
+    const resultDates = [...results.keys()];
+    snapshot.forEach((elm) => {
+      const data = elm.data()!;
+      sum += data.time;
+
+      // elmが一ヶ月以内のデータなら
+      if (resultDates.includes(elm.id)) {
+        results.set(elm.id, data.time);
+      }
+    });
+    this.resultTime = String(sum);
+    this.chartData = this.getSummaryData(results);
+    this.canRender = true;
+  }
+
+  public chartOptions(): Chart.ChartOptions {
+    return {
+      legend: {
+        display: false,
+      },
+    };
+  }
+
+  private getSummaryData(results: Map<string, number>): Chart.ChartData {
+    return {
+      labels: [...results.keys()],
+      datasets: [
+        {
+          label: 'Data One',
+          borderColor: '#009688',
+          backgroundColor: '#8BCFC8',
+          pointStyle: 'circle',
+          data: [...results.values()],
+        },
+      ],
+    };
+  }
+
+  private getInitializedResults(): Map<string, number> {
+    const date: Date = new Date();
+    const results: Map<string, number> = new Map<string, number>();
+
+    date.setDate(date.getDate() - 30);
+
+    for (let i = 0; i < 30; i++) {
+      results.set(this.formatDate(date), 0);
+      date.setDate(date.getDate() + 1);
+    }
+
+    return results;
+  }
+
+  private formatDate(date: Date): string {
+    const year = date.getFullYear().toString();
+    const month = this.putZeroPrefix((date.getMonth() + 1).toString(), 2);
+    const day = this.putZeroPrefix(date.getDate().toString(), 2);
+    return `${year}-${month}-${day}`;
+  }
+
+  private putZeroPrefix(str: string, length: number): string {
+    return ('0' + str).slice(-length);
   }
 }
 </script>
